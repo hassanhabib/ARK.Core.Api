648357e54f860fc7ce36ebce8d4a6091d287bfd5
Hassan Habib <hassanhabib@live.com>
2025-05-03 13:48:11 -0700

    ShouldReturnOKWithAllArksAsync -> FAIL

---
+++ ARK.Core.Api.Tests.Units/Controllers/Arks/ArksControllerTests.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using ARK.Core.Api.Controllers;
+using ARK.Core.Api.Models.ARKs;
+using ARK.Core.Api.Services.Foundations.Arks;
+using Moq;
+using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace ARK.Core.Api.Tests.Units.Controllers.Arks
+{
+    public partial class ArksControllerTests : RESTFulController
+    {
+        private readonly Mock<IArkService> arkServiceMock;
+        private readonly ArksController arksController;
+
+        public ArksControllerTests()
+        {
+            this.arkServiceMock =
+                new Mock<IArkService>();
+
+            this.arksController =
+                new ArksController(
+                    arkService: this.arkServiceMock.Object);
+        }
+
+        private IQueryable<Ark> CreateRandomArks()
+        {
+            return CreateArkFiller().Create(count: GetRandomNumber())
+                .AsQueryable();
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private Filler<Ark> CreateArkFiller()
+        {
+            var filler = new Filler<Ark>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().IgnoreIt();
+
+            return filler;
+        }
+    }
+}
---
+++ ARK.Core.Api/Controllers/ArksController.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using ARK.Core.Api.Models.ARKs;
+using ARK.Core.Api.Services.Foundations.Arks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ARK.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArksController : RESTFulController
+    {
+        private readonly IArkService arkService;
+
+        public ArksController(IArkService arkService) =>
+            this.arkService = arkService;
+
+        [HttpGet]
+        public async ValueTask<ActionResult<IQueryable<Ark>>> GetAllArksAsync() =>
+            NotImplemented("Endpoint not implemented");
+    }
+}
