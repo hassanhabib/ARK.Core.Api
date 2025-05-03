2f28018e4c84c7f7ef6f9e56b9ae32d72de81ccf
Hassan Habib <hassanhabib@live.com>
2025-05-03 10:58:10 -0700

    Add project files.

---
+++ ARK.Core.Api/Program.cs
@@ -0,0 +1,35 @@
+// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ARK.Core.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder webApplicationBuilder =
+                WebApplication.CreateBuilder(args);
+
+            webApplicationBuilder.Services.AddControllers();
+            webApplicationBuilder.Services.AddOpenApi();
+
+            WebApplication webApplication =
+                webApplicationBuilder.Build();
+
+            if (webApplication.Environment.IsDevelopment())
+            {
+                webApplication.MapOpenApi();
+            }
+
+            webApplication.UseHttpsRedirection();
+            webApplication.UseAuthorization();
+            webApplication.MapControllers();
+            webApplication.Run();
+        }
+    }
+}
