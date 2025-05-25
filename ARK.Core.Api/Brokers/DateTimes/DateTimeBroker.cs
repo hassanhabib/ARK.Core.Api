247d06abe6dfe289d2e83620bfa03777327d99d0
ZafarUrakov <zafarurakov@outlook.com>
2025-05-25 13:19:33 +0800

    BROKERS: Get Current Date Time Offset

---
+++ ARK.Core.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace ARK.Core.Api.Brokers.DateTimes
+{
+    internal class DateTimeBroker : IDateTimeBroker
+    {
+        public async ValueTask<DateTimeOffset> GetCurrentDateTimeOffsetAsync() =>
+            DateTimeOffset.UtcNow;
+    }
+}
